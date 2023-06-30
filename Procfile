6a3b379334fb220f1e9475097e9c6dbeb2bd2647
Christian Koranteng <korantengchristian@gmail.com>
2023-06-30 09:48:51 +0000

    Set up for production

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'justdo-app.wsgi'
--- justdo/settings.py
+++ justdo/settings.py
@@ -27,7 +27,7 @@ WEATHER_API_KEY = config('WEATHER_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
